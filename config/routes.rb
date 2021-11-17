f360f26c8950493b57c24b23be5f36846c7fb9d3
Thomas Chabanne <tchabanne@gmail.com>
2021-11-17 21:30:32 +0100

    les reviews ne s'affichent pas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'review/new'
+  get 'review/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :restaurants, only: [ :index, :new, :create, :show ]
+resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying"
+Restaurant.destroy_all
+puts "destroyed"
+puts "creating seeds"
+monsieur = Restaurant.new(name: 'Monsieur Machin', address: 'Nantes', category: 'french')
+monsieur.save
+ardoise = Restaurant.new(name: "L'ardoise", address: 'Nantes', category: 'french')
+ardoise.save
+maronnier = Restaurant.new(name: 'Les filles du marronier', address: 'Nantes', category: 'italian')
+maronnier.save
+deuxtours = Restaurant.new(name: "Les deux tours d'argent", address: 'Mordor', category: 'japanese')
+deuxtours.save
+elm = Restaurant.new(name: "Le gouffre d'Elm", address: 'Nantes', category: 'belgian')
+elm.save
+yummy = Review.new(content: "yummy yummy", rating: 4)
+yummy.restaurant = monsieur
+
+yummy2 = Review.new(content: "yummyyy yummyyyyyyYYYy", rating: 2)
+yummy2.restaurant = maronnier
+puts "seeds creates"
